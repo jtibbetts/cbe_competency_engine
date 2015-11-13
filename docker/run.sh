025efe2f64ffd514363288373ecda32abb06912f
Paul Gray <pfbgray@gmail.com>
2015-11-13 10:50:52 -0500

    Adding database migration task to startup script

--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,15 @@
 #
 # Starts the mysql database & runs the rails app.
 #
+
+
+#Setup the database
 service mysql start
 mysql --user="cbeuser" --password="cbepswd" --execute="CREATE DATABASE cbe_competency_engine;"
 mysql --user="cbeuser" --password="cbepswd" cbe_competency_engine < ./data/lti2_tc_mysql_init.sql
 
+#Migrate the database
+rake db:migrate RAILS_ENV=development
+
+#run the app
 rails s -p 4000

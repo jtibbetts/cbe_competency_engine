73eb91aa676092a0ace4381e2942592eb17a5866
Paul Gray <pfbgray@gmail.com>
2015-11-19 11:54:54 -0500

    Adding configurable host names

--- docker/run.sh
+++ docker/run.sh
@@ -2,6 +2,13 @@
 #
 # Starts the mysql database & runs the rails app.
 #
+[ -z "$HOST_PROTOCOL" ] && export HOST_PROTOCOL=http
+[ -z "$HOST_DOMAIN" ] && export HOST_DOMAIN=localhost
+[ -z "$HOST_PORT" ] && export HOST_PORT=4000
+
+#TODO: would like to support these parameters at some point
+#[ -z "$MYSQL_HOST" ] && export MYSQL_HOST=`/sbin/ip route|awk '/default/ { print $3 }'`
+#[ -z "$MYSQL_PORT" ] && export MYSQL_PORT=5984
 
 
 #Setup the database
@@ -9,6 +16,9 @@ service mysql start
 mysql --user="cbeuser" --password="cbepswd" --execute="CREATE DATABASE cbe_competency_engine;"
 mysql --user="cbeuser" --password="cbepswd" cbe_competency_engine < ./data/lti2_tc_mysql_init.sql
 mysql --user="cbeuser" --password="cbepswd" cbe_competency_engine < ./data/tcsampleapp.sql
+
+mysql --user="cbeuser" --password="cbepswd" --database "cbe_competency_engine" \
+      --execute="update lti2_tc_registries set content = '$HOST_PROTOCOL://$HOST_DOMAIN:$HOST_PORT' where name = 'tc_deployment_url'"
 
 #Migrate the database
 rake db:migrate RAILS_ENV=development

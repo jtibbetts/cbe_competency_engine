d99effce6566e6184128ca9c5d7d33a732642759
Paul Gray <pfbgray@gmail.com>
2015-11-21 10:31:38 -0500

    Adding HOST environment variable to Dockerfile

--- docker/run.sh
+++ docker/run.sh
@@ -5,6 +5,7 @@
 [ -z "$HOST_PROTOCOL" ] && export HOST_PROTOCOL=http
 [ -z "$HOST_DOMAIN" ] && export HOST_DOMAIN=localhost
 [ -z "$HOST_PORT" ] && export HOST_PORT=4000
+[ -z "$HOST" ] && export HOST="$HOST_PROTOCOL://$HOST_DOMAIN:$HOST_PORT"
 
 #TODO: would like to support these parameters at some point
 #[ -z "$MYSQL_HOST" ] && export MYSQL_HOST=`/sbin/ip route|awk '/default/ { print $3 }'`
@@ -18,7 +19,7 @@ mysql --user="cbeuser" --password="cbepswd" cbe_competency_engine < ./data/lti2_
 mysql --user="cbeuser" --password="cbepswd" cbe_competency_engine < ./data/tcsampleapp.sql
 
 mysql --user="cbeuser" --password="cbepswd" --database "cbe_competency_engine" \
-      --execute="update lti2_tc_registries set content = '$HOST_PROTOCOL://$HOST_DOMAIN:$HOST_PORT' where name = 'tc_deployment_url'"
+      --execute="update lti2_tc_registries set content = '$HOST' where name = 'tc_deployment_url'"
 
 #Migrate the database
 rake db:migrate RAILS_ENV=development
